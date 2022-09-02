fe216f2a4b57580135132f7092fb30d8712518db
Tom Corcoran <tomcorcoran@Toms-MBP.hub>
2022-09-03 09:43:15 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# nodejs-websocket-3scale
+# Nodejs Websocket 3scale
+
+An example Websocket API protected by 3scale, deployed on OpenShift Container Platform
+Based on git hub repo ajb413 nodejs-websocket-examples. Thanks guys.
+
+## Install API Locally
+
+```
+git clone https://github.com/tnscorcoran/nodejs-websocket-3scale
+cd nodejs-websocket-3scale
+export REPO_HOME=`pwd`
+```
+
---
+++ websocket-server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const WebSocketServer = require('websocket').server;
+
+const server = http.createServer();
+// _tc server.listen(9898);
+server.listen(8080);
+
+const wsServer = new WebSocketServer({
+    httpServer: server
+});
+
+wsServer.on('request', function(request) {
+    const connection = request.accept(null, request.origin);
+
+    connection.on('message', function(message) {
+      console.log('Received Message:', message.utf8Data);
+      //connection.sendUTF('Hi this is WebSocket server!');
+
+
+      function logEvery2Seconds(i) {
+        setTimeout(() => {
+            var message = 'Message '+i+' from the Node JS WebSocket server!';
+            
+            console.log(message);
+            
+            connection.sendUTF(message);
+
+            logEvery2Seconds(++i);
+        }, 4000)
+      }
+
+      logEvery2Seconds(1);
+
+
+
+
+
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log('Client has disconnected.');
+    });
+});
